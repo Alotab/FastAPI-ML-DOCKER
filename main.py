056429e4f69be478edf7a1a3cbd2a8064ac0a851
alotab <alotab@yahoo.com>
2023-09-08 04:00:26 +0000

    add fastapi and model from hugging face

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from model import model_pipeline
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+import io
+from PIL import Image
 
 app = FastAPI()
 
 @app.get("/")
 def read_get():
-    return {"message": "hELLO WORLD"}
+    return {"message": "HELLO WORLD"}
 
+
+
+@app.post("/ask")
+def ask(text: str, image: UploadFile):
+    # read file from the uploadfile image
+    content = image.file.read()
+
+    image = Image.open(io.BytesIO(content))
+    # image = Image(image.file)
+
+    result = model_pipeline(text, image)
+
+    return {"result": result}
